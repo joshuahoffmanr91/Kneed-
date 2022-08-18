5e0981ae548ad44fec0b016a1a65afca967bfa8e
joshuahoffmanr91 <108317640+joshuahoffmanr91@users.noreply.github.com>
2022-08-18 12:58:50 -0500

    Update Compression_Alpha_1-3.bash
    Rework file tab and whitespace parse.
    Refactor array declarations.
    Decompress rebuilds as string path is unfolded.

--- Compression_Alpha_1-3.bash
+++ Compression_Alpha_1-3.bash
@@ -1,62 +1,57 @@
 #!/bin/bash
 
+shopt -s extglob
 
 Compress () {
 
-shopt -s extglob
-export N="$(cat $@)"
-S_=(${N})
-declare -g A_=(${S_[@]//?()/\\})
-eval eval declare -Ag B_[\\\${A_[{0..${#A_[@]}}]}-]+=\"\\\ $\[A++\]\"
-D_=(${!B_[@]})
-eval eval declare -ag C_[\\\${#D_[{0..${#D_[@]}}]}]+=\"\\\ \\\${D_[B++]}\"
-E_=(${C_[@]})
-eval eval declare -g F_[C++]=\\\${E_[{${#E_[@]}..0}]}
+N=" $(cat $1) " && N="${N//$'\n'/\ $'\n'\ }" && N="${N//$'\t'/    }"
+S_=(${N}) && A_=(${S_[@]//?()/\\})
+eval eval declare -A B_[\\\${A_[{0..${#A_[@]}}]%\\}]+=\"\\\ $\[A++\]\"
 
-        for b in ${F_[@]%-}; do
+        for b in ${!B_[@]}; do
 
-                for c in ${B_[$b-]}; do
+                for c in ${B_[$b]}; do
                 ! [[ " ${H_[@]} " == *" ${A_[--A-1]} "* ]] && H_+=("${A_[A-1]}")
-                [[ -z $e ]] && eval {d,e}=\$c && continue
-                N="${N/"$b"/$[c-d]}"
+                [[ -z $e ]] && e=$c && continue
+                N="${N/" $b "/\ $[c-${d:=e}]\ }"
                 d=$c
                 done
 
-        N="${N/"$b"/$e-}"
+        N="${N/" $b "/\ 0$e\ }"
         unset d e
         done
 
-eval echo ${H_[@]} > $1.test
-cat <<<"$N" >> $1.test
+eval echo ${H_[@]} "\"
+$N\"" > $1.isf
 
 }
 
-
 Decompress () {
 
-Stand="$(cat $@)"
+shopt -s extglob
+unset {A..Z}
 mapfile Jump < "$@"
 Slip=(${Jump[0]})
-Skip=(${Jump[@]:1:${#Jump[@]}})
+Skip=(${Jump[@]:1})
+Runs="${Jump[@]:1}" && eval Runs=\""${Runs//+([0-9])/\$[B++]}"\"
 
         for (( Road=0; ${#Skip[@]} >= Road; Road++ )); do
-        [[ ${Skip[Road]//[!-]} =~ - ]] || continue && ((A++)) &&\
-        [[  ${Skip[Road]/-} == $Road ]] && Leap[Road]=${Slip[-A+1]} &&\
-        continue || Path=${Skip[Road]/-}
+        [[ ${Skip[Road]::1} == 0 ]] || continue && ((++A)) &&\
+        [[  ${Skip[Road]#1} == $Road ]] && Runs="${Runs/$Walk/${Slip[-A]}}" &&\
+        continue || Path=${Skip[Road]#0}
 
-                until (( Walk == Road )); do
+                until (( ${Walk:=0} == $Road )); do
                 Path=${Skip[$((Walk+=Path))]/-}
-                Leap[Walk]=${Slip[-A+1]}
+                Runs="${Runs/$Walk/${Slip[-A]}}"
                 done
 
         unset Path Walk
         done
 
-Stand="${Jump[@]:1}"
-eval Runs="\"${Stand//+([0-9|-])/\$\{Leap\[\$\(\(Z++\)\)\]\}}\""
-cat <<<"$Runs" >> $1
+echo "${Runs}"
 
 }
 
-Compress $@
-Decompress $@.test
+
+Compress $1
+Decompress $1.isf
